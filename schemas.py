d34c1abe86a6491e436694308e902bf71cecebd7
Karan Chauhan <karan.chauahn.btech2023@sitpune.edu.in>
2026-04-08 13:02:10 +0530

    schemas made for events

--- schemas.py
+++ schemas.py
@@ -38,3 +38,79 @@ class CoachResponse(BaseModel):
     plan: str
 
 
+# ---------- Events ----------
+
+class EventCreate(BaseModel):
+    eventType: str
+    eventDate: date
+    eventTime: time
+    location: str
+    description: str
+
+
+class EventResponse(BaseModel):
+    id: int
+    eventType: str
+    eventDate: date
+    eventTime: time
+    location: str
+    description: str
+
+
+class FitnessRequest(BaseModel):
+    age: int
+    height: float   # in cm
+    weight: float   # in kg
+    gender: Literal["Male", "Female", "Other"]
+
+
+class FitnessResponse(BaseModel):
+    bmi: float
+    bmr: float
+    fitness_recommendations: str
+    diet_recommendations: str
+
+class TeamCreate(BaseModel):
+    team_name: str
+    state: str
+    district: str
+
+
+class PlayerAdd(BaseModel):
+    team_name: str
+    player_name: str
+    role: str
+    contact: str
+    email: str
+
+
+class JoinRequest(BaseModel):
+    team_name: str
+    player_name: str
+
+
+class PlayerResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+
+
+class PlayerStatsCreate(BaseModel):
+    player_id: int
+    goals_scored: int
+    assists: int
+    other_metrics: Optional[str] = None
+
+
+class PlayerStatsResponse(BaseModel):
+    id: int
+    player_id: int
+    goals_scored: int
+    assists: int
+    other_metrics: Optional[str]
+
+
+class FeedbackCreate(BaseModel):
+    name: str
+    content: str
